4e57093706a4e1743e5078727324ce6a804a97b2
Karolis Lasys (aka. HyperWind) <karolioofficial@outlook.com>
2015-01-19 20:48:06 +0200

    fixed a bug

--- init.zsh
+++ init.zsh
@@ -2,8 +2,9 @@
 
 ##
 # $1 is the path to the init.zsh file
-# $2 is the plugin string
-# $3 is the theme
+# $2 is 1 if the user wants to load any configs
+# $3 is the plugin string or 0 if it's empty
+# $4 is the theme or 0 if it's empty
 #
 # This configuration doesn't change for any other bundle.
 # Also, every custom init.zsh file should have an init_custom_bundle function, otherwise the bundle won't be loaded.
@@ -16,8 +17,8 @@ init_custom_bundle() {
 
 	BUNDLE="$1/oh-my-zsh"
 
-	[[ -z "$1" ]] || load_configs lib
-	[[ -z "$2" ]] || load_plugins $2 plugins
-	[[ -z "$3" ]] || load_themes $3 themes
+	[[ $2 != 0 ]] && load_configs lib
+	[[ $3 != 0 ]] && load_plugins $3 plugins
+	[[ $4 != 0 ]] && load_themes $4 themes
 
 }
